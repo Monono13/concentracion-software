dc453d6b60fcc4a99b29fcbe2ac06ebbed72624e
Luis Cividanes <57505286+Monono13@users.noreply.github.com>
2023-02-14 19:15:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ app = Flask(__name__, template_folder='template')
 @app.route('/')
 def home():
     return render_template('home.html')
+@app.route('/ProccessUserInfo/<string:username>', methods=['POST'])
+def ProccessUserInfo(username):
+    username = json.loads(username)
+    return('/')
+
+@app.route('/ProccessUserInfo/<string:password>', methods=['POST'])
+def ProccessUserInfo(password):
+    password = json.loads(password)
+    return('/')
 
 if __name__ == '__main__':
     app.run()
